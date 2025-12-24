ca7bae9be2c9613483ca65f2552fc25060ede963
Okan <c.okan@byondx.app>
2025-12-24 13:21:21 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,7 @@ export default defineConfig(({ mode }) => {
       react()
     ],
     define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      // 'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
